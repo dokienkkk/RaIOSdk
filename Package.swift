74a4fbce8c66ffd1afbbbdaeff50ec1955d6a5ae
docongkien <docongkien1999@gmail.com>
2026-07-03 13:23:39 +0700

    Release 0.0.9

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RaIOSdkBinary",
-            url: "https://github.com/dokienkkk/RaIOSdk/releases/download/0.0.8/RaIOSdk-0.0.8.xcframework.zip",
-            checksum: "22420334319dbdfd18587f869a883ab5c2dcddc6fe8629d1febdad8f8caff44b"
+            url: "https://github.com/dokienkkk/RaIOSdk/releases/download/0.0.9/RaIOSdk-0.0.9.xcframework.zip",
+            checksum: "c8a478f9e17da8473280243308fbebc0ff149cc4bd3170f801493353774b8dbc"
         ),
         .target(
             name: "RaIOSdkWrapper",
